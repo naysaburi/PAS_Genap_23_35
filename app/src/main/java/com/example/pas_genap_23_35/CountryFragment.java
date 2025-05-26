6a5515a9ba02c7625ca15d63386dff93a6a6bde3
Dvonne-D-Thales <05566@smkrus.sch.id>
2025-05-26 08:58:41 +0700

    xml setup and fragment

---
+++ app/src/main/java/com/example/pas_genap_23_35/CountryFragment.java
@@ -0,0 +1,13 @@
+package com.example.pas_genap_23_35;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CountryFragment extends Fragment {
+
+}
