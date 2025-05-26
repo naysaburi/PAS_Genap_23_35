5c91cbcad3370bcc3dbd23914fa12d24f860f9a5
naysaburi <diggiepier550@gmail.com>
2025-05-26 09:49:38 +0700

    Update Logic

--- app/src/main/java/com/example/pas_genap_23_35/MainActivity.java
+++ app/src/main/java/com/example/pas_genap_23_35/MainActivity.java
@@ -1,19 +1,51 @@
 package com.example.pas_genap_23_35;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
+
+    FrameLayout frameLayout;
+    BottomNavigationView bottomNavigationView;
+    private SpainFragment spainFragment = new SpainFragment();
+    private CountryFragment countryFragment = new CountryFragment();
+    private ProfileFragment profileFragment = new ProfileFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        frameLayout = findViewById(R.id.flFragment);
+        bottomNavigationView = findViewById(R.id.bottomNav);
+        bottomNavigationView.setOnItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.spainTeam);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.spainTeam) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, spainFragment).commit();
+        } else if (id == R.id.country) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, countryFragment).commit();
+        } else if (id == R.id.profile) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, profileFragment).commit();
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/pas_genap_23_35/SpainFragment.java
+++ app/src/main/java/com/example/pas_genap_23_35/SpainFragment.java
@@ -2,12 +2,85 @@ package com.example.pas_genap_23_35;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SpainFragment extends Fragment {
+    SwipeRefreshLayout swipeRefreshLayout;
+    RecyclerView rvSpain;
+    private SpainAdapter spainAdapter;
+    private List<SpainModel> spainModels = new ArrayList<>();
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_spain, container, false);
+
+        swipeRefreshLayout = view.findViewById(R.id.swipeRefreshSpain);
+        rvSpain = view.findViewById(R.id.rvSpain);
+
+        spainAdapter = new SpainAdapter(spainModels);
+        rvSpain.setLayoutManager(new LinearLayoutManager(getContext()));
+        rvSpain.setAdapter(spainAdapter);
+
+        swipeRefreshLayout.setOnRefreshListener(this::refreshData);
+
+        return view;
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        refreshData();
+    }
+
+    private void refreshData() {
+        spainModels.clear();
+        spainAdapter.notifyDataSetChanged();
+        fetchTeams();
+    }
+
+    private void fetchTeams() {
+        swipeRefreshLayout.setRefreshing(true);
+
+        ApiService apiService = ApiClient.getApiService();
+        Call<SpainResponse> call =apiService.getAllSpain();
+
+        call.enqueue(new Callback<SpainResponse>() {
+            @Override
+            public void onResponse(Call<SpainResponse> call, Response<SpainResponse> response) {
+                swipeRefreshLayout.setRefreshing(false);
+
+                if (response.isSuccessful() && response.body() != null) {
+                    spainModels.addAll(response.body().getSpain());
+                    spainAdapter.notifyDataSetChanged();
+                } else {
+                    Toast.makeText(getContext(), "Gagal ambil data: " + response.code(), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<SpainResponse> call, Throwable t) {
+                swipeRefreshLayout.setRefreshing(false);
+                Toast.makeText(getContext(), "Failed: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
